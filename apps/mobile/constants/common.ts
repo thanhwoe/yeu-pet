a215c47140f2c6aad195090a7ba532b1b8582c34
Thanh <thanh@Thanhs-MacBook-Air.local>
2026-05-26 09:15:43 +0700

    Update mobile env

--- apps/mobile/constants/common.ts
+++ apps/mobile/constants/common.ts
@@ -9,14 +9,14 @@ import { Platform } from "react-native";
 // };
 
 export const ENV = {
-  API_URL: __DEV__
-    ? Platform.select({
-        // ios: "https://pet-land-api.thanhwoe.workers.dev/api",
-        ios: "http://localhost:3000/api/v1",
-        // ios: "http://10.10.10.183:3000/api/v1",
-        android: "http://10.0.2.2:8787/api",
-        default: "http://localhost:8787/api",
-      })
-    : "https://pet-land-api.thanhwoe.workers.dev/api",
-  GEMINI_API_KEY: "AIzaSyCE0ThLoZmmu-h6-P7aFuewvB19RjFIlZY",
+  API_URL:
+    process.env.EXPO_PUBLIC_API_URL ||
+    Platform.select({
+      ios: "http://localhost:3000/api/v1",
+      android: "http://10.0.2.2:3000/api/v1",
+      default: "http://localhost:3000/api/v1",
+    }),
+  GEMINI_API_KEY:
+    process.env.EXPO_PUBLIC_GEMINI_API_KEY ||
+    "AIzaSyCE0ThLoZmmu-h6-P7aFuewvB19RjFIlZY",
 };
